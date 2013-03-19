aab8e954f5e6d82874aa58f62ce8ea3f4111a8c9
helleste <heller.stepan@gmail.com>
2013-03-19 23:30:36 +0100

    adding src folder

---
+++ src/robot/Robot.java
@@ -0,0 +1,327 @@
+// Author: Štěpán Heller (helleste)
+
+package robot;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.DatagramSocket;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Robot {
+	
+	public static void main(String[] args) {
+		if(args.length == 0) {
+			System.out.println("Usage/Photo: java robot.Robot <hostname>");
+			System.out.println("Usage/Firmware: java robot.Robot <hostname> <firmware.bin>");
+		} else if(args.length == 1) {
+			PhotoClient photoClient = new PhotoClient(args[0]);
+			photoClient.run();
+		} else if(args.length == 2) {
+//			Client client = new Client(args[0], Integer.parseInt(args[1]));
+//			client.run();
+		}
+	}
+}
+
+class PhotoClient {
+	
+	private DatagramSocket socket;
+	private DatagramPacket packet;
+	private static final int PORT = 4000; // port number on baryk
+	private InetAddress address = null;
+	
+	private static final int SYN = 1; // SYN flag
+	private static final int FIN = 4; // FIN flag
+	private static final int RST = 2	; // RST flag
+	
+	private static final byte[] DOWNLOAD = {0x01}; // Download a photo
+	
+	private ArrayList<Boolean> flags = new ArrayList<Boolean>(); // Array of flags according to received packets
+	private int lastAck = 0; // Last send ACK
+	
+	private ArrayList<PhotoPacket> receivedPackets = new ArrayList<PhotoPacket>(); // Raw photo data
+	
+	public PhotoClient(String host) {
+		try {
+			this.address = InetAddress.getByName(host);
+			this.socket = new DatagramSocket();
+		}
+		catch(UnknownHostException e) {
+			System.out.println("Unknown host. Exiting.");
+			System.exit(-1);
+		}
+		catch (SocketException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void run() {
+		
+		PhotoPacket ppacket = null;
+		
+		try {
+			ppacket = init(ppacket);
+			
+			// Receive second packet from baryk with real photo data
+			this.packet = new DatagramPacket(new byte[265], 265, this.address, PORT);
+			this.socket.receive(this.packet);
+			ppacket = new PhotoPacket(this.packet.getData());
+			ppacket.printPacket();
+			// TODO Save photo data
+			
+			int index = 0;
+			int ack = 0;
+			
+			while(!ppacket.fin) {
+				index = calcIndex(ppacket.seqNum);
+				setSign(index); // Set flag in the array from received seqNum
+				savePacket(index, ppacket); // Save PhotoPacket to the array
+				ack = findAck(); // Find ACK to send
+				
+				// Send confirmation packet to baryk
+				ppacket = new PhotoPacket(ppacket.conNum, 0, ack, 0, null);
+				packPacket(ppacket);
+				ppacket.printPacket();
+				this.socket.send(this.packet);
+				
+				// Receive new packet
+				this.packet = new DatagramPacket(new byte[265], 265, this.address, PORT);
+				this.socket.receive(this.packet);
+				ppacket = new PhotoPacket(this.packet.getData());
+				ppacket.printPacket();
+			}
+			
+			// We have ppacket with fin flag on
+			// TODO Send FIN packet to baryk
+			// TODO Save photo
+			System.out.println("RECEIVING DATA FINISHED!");
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	// Establish communication with baryk
+	private PhotoPacket init(PhotoPacket ppacket) throws IOException {
+		
+		ppacket = new PhotoPacket(0, 0, 0, SYN, DOWNLOAD);
+		packPacket(ppacket);
+		ppacket.printPacket();
+		
+		// Send packet to baryk
+		this.socket.send(this.packet);
+		
+		// Receive packet from baryk
+		this.socket.receive(this.packet);
+					
+		// Create PhotoPacket from datagram
+		ppacket = new PhotoPacket(this.packet.getData());
+		
+		// Print header of received packet
+		ppacket.printPacket();
+		
+		return ppacket;
+	}
+	
+	// Fill my DatagramPacket with my custom PhotoPacket's data
+	public void packPacket(PhotoPacket ppacket) {
+		
+		byte myPacket[] = new byte[10];
+		
+		// Add connection number first
+		myPacket[0] = (byte) (ppacket.conNum >> 24);
+		myPacket[1] = (byte) (ppacket.conNum >> 16);
+		myPacket[2] = (byte) (ppacket.conNum >> 8);
+		myPacket[3] = (byte) (ppacket.conNum);
+		
+		// Then add the sequence number
+		myPacket[4] = (byte) (ppacket.seqNum >> 8);
+		myPacket[5] = (byte) (ppacket.seqNum);
+		
+		// Then add the acknowledgment number
+		myPacket[6] = (byte) (ppacket.ackNum >> 8);
+		myPacket[7] = (byte) (ppacket.ackNum);
+		
+		// The header ends with set of flags
+		myPacket[8] = (byte) (ppacket.signs);
+		
+		// At the end we fill the packet with data
+		if(ppacket.data != null) {
+			for (int i = 0; i < ppacket.data.length; i++) {
+				myPacket[9 + i] = ppacket.data[i];
+			}
+		}
+		
+		// Create the final DatagramPacket
+		this.packet = new DatagramPacket(myPacket, myPacket.length, this.address, PORT);
+		
+	}
+	
+	// Calculate index from received seqNum
+	private int calcIndex(int seqNum) {
+		
+		return seqNum/255; 
+	}
+	
+	// Set flag in the array according to received index
+	private void setSign(int index) {
+		
+		if(index >= this.flags.size()) {
+			for (int i = this.flags.size(); i < index; i++) {
+				this.flags.add(i, false);
+			}
+		}
+		
+		this.flags.set(index, true);
+		return;
+	}
+	
+	// Add received packet to the array
+	private void savePacket(int index, PhotoPacket photoPacket) {
+		
+		if(index >= this.receivedPackets.size()) {
+			for (int i = this.receivedPackets.size(); i < index; i++) {
+				this.receivedPackets.add(i, null);
+			}
+		}
+		
+		this.receivedPackets.set(index, photoPacket);
+		return;
+	}
+	
+	// Find ACK to send
+	private int findAck() {
+		int ack = 255;
+		
+		for(Iterator<Boolean> it = this.flags.iterator(); it.hasNext(); ack += 255) {
+			Object element = it.next();
+			if(element.equals(false)) { // I find a hole in the array
+				ack -= 255;
+				break;
+			}
+		}
+		
+		return ack;
+	}
+}
+
+class PhotoPacket {
+	
+	public static final int HEADER_LENGTH = 9;
+	
+	public int conNum; // connection number from Robot after SYN
+	public int seqNum; // sequence number
+	public int ackNum; // acknowledgment number
+	public int signs; // signs (SYN,FIN,RST)
+	public boolean syn; // SYN sign
+	public boolean fin; // FIN sign
+	public boolean rst; // RST sign
+	public byte[] data; // photo data
+	
+	// Create new PhotoPacket with all parameters
+	public PhotoPacket(int conNum, int seqNum, int ackNum, int signs, byte[] data) {
+		
+		this.conNum = conNum;
+		this.seqNum = seqNum;
+		this.ackNum = ackNum;
+		this.signs = signs;
+		this.data = data;
+		parseSigns(signs);
+	}
+	
+	// Create new PhotoPacket from received datagram
+	public PhotoPacket(byte datagram[]) {
+		
+		this.conNum = (bToI(datagram[0]) << 24) + (bToI(datagram[1]) << 16)
+				+ (bToI(datagram[2]) << 8) + bToI(datagram[3]);
+		this.seqNum = (bToI(datagram[4]) << 8) + bToI(datagram[5]);
+		this.ackNum = (bToI(datagram[6]) << 8) + bToI(datagram[7]);
+		this.signs = bToI(datagram[8]);
+		parseSigns(signs);
+		
+		int dataLength = datagram.length - HEADER_LENGTH;
+		this.data = new byte[dataLength];
+		
+		for(int i = 0; i < dataLength; i++) {
+			this.data[i] = datagram[HEADER_LENGTH + i];
+		}
+	}
+	
+	// Convert byte to int
+	private int bToI(byte b) {
+		
+		return b & 0xFF;
+	}
+	
+	// Set flags from signs
+	private void parseSigns(int signs) {
+		
+		switch (signs) {
+		case 0:
+			this.syn = false;
+			this.fin = false;
+			this.rst = false;
+			break;
+		case 1:
+			this.rst = false;
+			this.syn = true;
+			this.fin = false;
+			break;
+		case 2:
+			this.fin = false;
+			this.syn = false;
+			this.rst = true;
+			break;
+		case 4:
+			this.syn = false;
+			this.fin = true;
+			this.rst = false;
+			break;
+
+		default:
+			System.out.println("Reached default in parseSigns. This should never happen.");
+			break;
+		}
+	}
+	
+	// Print PhotoPacket's data
+	public void printPacket() {
+		
+		StringBuilder log = new StringBuilder();
+		log.append("CONNECTION NUMBER: " + this.conNum);
+		log.append(" SEQUENCE NUMBER: " + this.seqNum);
+		log.append(" SIGNS: " + this.signs);
+		log.append (" ACK: " + this.ackNum);
+		log.append(" SYN: " + this.syn);
+		log.append(" FIN: " + this.fin);
+		log.append(" RST: " + this.rst);
+		if(this.data != null) {
+			log.append(" DATA LENGHT: " + this.data.length);
+			log.append(" DATA: " + bytesToHex(this.data));
+		}
+		
+		System.out.println(log.toString());
+	}
+	
+	// Create string from data's hexa
+	// Inspiration from: http://stackoverflow.com/questions/9655181/convert-from-byte-array-to-hex-string-in-java
+	public static String bytesToHex(byte[] bytes) {
+		
+		final char[] hexArray = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+		char[] hexChars = new char[bytes.length * 2];
+		int v;
+		
+		for (int i = 0; i < bytes.length; i++) {
+			v = bytes[i] & 0xFF;
+			hexChars[i * 2] = hexArray[v >>> 4];
+			hexChars[i * 2 + 1] = hexArray[v & 0x0F];
+		}
+		
+		return new String(hexChars);
+	}
+}
